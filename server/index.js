ddbc7ef52f7dc225551db6e631cfe43473b453fc
Jitsrcpresident <100992088+Jitsrcpresident@users.noreply.github.com>
2024-10-23 20:04:54 +0530

    URL update frontend

--- server/index.js
+++ server/index.js
@@ -15,8 +15,8 @@ const orderRouter = require('./routes/order.routes');
 app.use(express.json());
 
 const corsOptions = {
-    origin: '*', // Replace with your frontend's origin
-    methods: ['GET', 'POST', 'PUT','PATCH', 'DELETE'], // Allowed HTTP methods
+    origin: process.env.FRONT_END_URL, // Replace with your frontend's origin
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], // Allowed HTTP methods
     credentials: true, // Allow cookies and authentication headers
 };
 
