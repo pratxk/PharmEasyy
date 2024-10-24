f0e9b4a84e9deab47b1f711ec8950cef29beab8d
pratxk <pratxk007@gmail.com>
2024-10-24 09:22:38 +0530

    ENV Update

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 4000 + Math.floor(Math.random() * 1000);
 app.use(express.json());
 
 const corsOptions = {
-    origin: 'https://pharm-easyy.vercel.app', // Replace with your frontend's origin
+    origin: process.env.FRONT_END_URL, // Replace with your frontend's origin
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], // Allowed HTTP methods
     credentials: true, // Allow cookies and authentication headers
 };
